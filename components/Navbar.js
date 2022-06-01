2794c3625efd601eddc2d371460225faa8b31377
Shivam <shivamsnaik@gmail.com>
2022-06-01 09:29:56 +0000

    Fix navbar Links responsive behaviour.
    - Fix issue of menu item staying in flex-direction: column, when in mobile view, menu button is clicked and zoomed out to desktop mode.

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,19 +2,23 @@ import styles from '../styles/Navbar.module.css'
 import button, { useEffect, useState } from 'react'
 export default function Navbar({ children }) {
 
-    const [menu_toggle, toggleMenu] = useState(false)
+    const [init_status, setAppStartStatus] = useState(true)
+    const [menu_toggle, toggleMenu] = useState(true)
 
-    useEffect(()=> {
-
-    });
 
     function menuClick(e) {
         e.preventDefault();
+        setAppStartStatus(false)
         toggleMenu(!menu_toggle)
         console.log("Button Clicked")
-    }
+    };
 
-    return ( 
+    function navMenuClicked(e){
+        e.preventDefault();
+        console.log('Menu Button Clicked')
+    };
+
+    return (
         <div className={styles.navbar}>
             <div className={styles.content}>
                 <div className={styles.navbar_menus}>
@@ -24,13 +28,12 @@ export default function Navbar({ children }) {
                 </div>
                 <div className={`${styles.navbar_links}`}>
                     <button onClick={menuClick} style={{padding:"5px"}} className={styles.logo}><img src="/menu-button.svg" alt="Menu" className={styles.logoimg}/></button>
-                    <div className={styles.link_items} style={{display: menu_toggle?'flex':'none'}}>
-                        <div><a href="">Academics</a></div>
-                        <div><a href="">Work</a></div>
-                        <div><a href="">About</a></div>
-                        <div><a href="">Resume</a></div>
+                    <div className={menu_toggle?styles.link_items:styles.link_items_toggled}>
+                        <div onClick={navMenuClicked}><p>Academics</p></div>
+                        <div onClick={navMenuClicked}><p>Work</p></div>
+                        <div onClick={navMenuClicked}><p>About</p></div>
+                        <div onClick={navMenuClicked}><p>Resume</p></div>
                     </div>
-                    
                 </div>
             </div>
         </div>
